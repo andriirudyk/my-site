5a5e0503c245e51b3ea53c02111d488a95dbf221
Z0d1akAllSeeingEye <andriirudyk32@gmail.com>
2026-08-03 03:10:20 +0300

    uploading the project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Header} from './components/header/header';
+import {Hero} from './components/hero/hero';
+import {Footer} from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Hero, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
